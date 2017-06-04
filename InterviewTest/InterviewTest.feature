cdd75b78af368fd560db53952a916a46c271101c
halimi <halimi@freemail.hu>
2017-06-04 20:23:05 +0200

    Add feature file and test steps

---
+++ InterviewTest/InterviewTest.feature
@@ -0,0 +1,103 @@
+﻿Feature: InterviewTest
+	In order to use the web page
+	As a user
+	I want to specify the web page's behaviour
+
+
+Scenario Outline: REQ-UI-01 The Title should be "UI Testing Site" on every site
+	Given user is on the landing page
+	And the <Button> is available
+	When user clicks to the <Button>
+	Then the <Page> loads
+	And the title is "UI Testing Site"
+	Examples: 
+	| Button            | Page      |
+	| UI Testing button | Home page |
+	| Home button       | Home page |
+	| Form button       | Form page |
+
+Scenario Outline: REQ-UI-02 The Company Logo should be visible on every site
+	Given user is on the landing page
+	And the <Button> is available
+	When user clicks to the <Button>
+	Then the <Page> loads
+	And The Company Logo is visible on the page
+	Examples: 
+	| Button            | Page      |
+	| UI Testing button | Home page |
+	| Home button       | Home page |
+	| Form button       | Form page |
+
+Scenario: REQ-UI-03 When I click on the Home button, I should get navigated to the Home page
+	Given user is on the Form page
+	And the Home button is available
+	When user clicks to the Home button
+	Then the Home page loads
+
+Scenario: REQ-UI-04 When I click on the Home button, it should turn to active status
+	Given user is on the Form page
+	And the Home button is available
+	When user clicks to the Home button
+	Then the Home button is active
+
+Scenario: REQ-UI-05 When I click on the Form button, I should get navigated to the Form page
+	Given user is on the Home page
+	And the Form button is available
+	When user clicks to the Form button
+	Then the Form page loads
+
+Scenario: REQ-UI-06 When I click on the Form button, it should turn to active status
+	Given user is on the Home page
+	And the Form button is available
+	When user clicks to the Form button
+	Then the Form button is active
+
+Scenario: REQ-UI-07 When I click on the Error button, I should get a 404 HTTP response code
+	Given user is on the Home page
+	And the Error button is available
+	When user clicks to the Error button
+	Then user get a 404 HTTP response code
+
+Scenario: REQ-UI-08 When I click on the UI Testing button, I should get navigated to the Home page
+	Given user is on the Form page
+	And the UI Testing button is available
+	When user clicks to the UI Testing button
+	Then the Home page loads
+
+Scenario: REQ-UI-09 The following text should be visible on the Home page in <h1> tag:
+"Welcome to the Docler Holding QA Department"
+	Given user is on the Form page
+	And the Home button is available
+	When user clicks to the Home button
+	Then the Home page loads
+	And "Welcome to the Docler Holding QA Department" text is visible on the Home page in h1 tag
+
+Scenario: REQ-UI-10 The following text should be visible on the Home page in <p> tag:
+"This site is dedicated to perform some exercises and demonstrate automated web testing."
+	Given user is on the Form page
+	And the Home button is available
+	When user clicks to the Home button
+	Then the Home page loads
+	And "This site is dedicated to perform some exercises and demonstrate automated web testing." text is visible on the Home page in p tag
+
+Scenario: REQ-UI-11 On the Form page, a form should be visible with one input box and one submit button
+	Given user is on the Home page
+	And the Form button is available
+	When user clicks to the Form button
+	Then the Form page loads
+	And a form is visible
+	And one input box is visible on the page
+	And one submit button is visible on the page
+
+Scenario Outline: REQ-UI-12 On the Form page, if you type <value> the input field and submit the form, you should get redirect to the Hello page, and the following text should appear: <result>
+	Given user is on the Form page
+	When user type <Value> to the input field
+	And submit the form
+	Then the page redirected to the Hello page
+	And the following text is appear <Result>
+	Examples: 
+	| Value   | Result         |
+	| John    | Hello John!    |
+	| Sophia  | Hello Sophia!  |
+	| Charlie | Hello Charlie! |
+	| Emily   | Hello Emily!   |
---
+++ InterviewTest/InterviewTestSteps.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace InterviewTest
+{
+    [Binding]
+    public class InterviewTestSteps
+    {
+
+        IWebDriver driver;
+        IWebElement testButton;
+        IWebElement homeButton;
+        IWebElement formButton;
+        IWebElement errorButton;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Given(@"user is on the Form page")]
+        public void GivenUserIsOnTheFormPage()
+        {
+            driver.Url = "http://uitest.duodecadits.com/form.html";
+        }
+        
+        [Given(@"user is on the landing page")]
+        [Given(@"user is on the Home page")]
+        public void GivenUserIsOnTheHomePage()
+        {
+            driver.Url = "http://uitest.duodecadits.com";
+        }
+
+        [Given(@"the Home button is available")]
+        public void GivenTheHomeButtonIsAvailable()
+        {
+            homeButton = driver.FindElement(By.XPath("//a[@id='home']"));
+        }
+
+        [Given(@"the UI Testing button is available")]
+        public void GivenTheUITestingButtonIsAvailable()
+        {
+            testButton = driver.FindElement(By.XPath("//a[@id='site']"));
+        }
+
+        [Given(@"the Form button is available")]
+        public void GivenTheFormButtonIsAvailable()
+        {
+            formButton = driver.FindElement(By.XPath("//a[@id='form']"));
+        }
+        
+        [Given(@"the Error button is available")]
+        public void GivenTheErrorButtonIsAvailable()
+        {
+            errorButton = driver.FindElement(By.XPath("//a[@id='error']"));
+        }
+        
+        [When(@"user clicks to the Home button")]
+        public void WhenUserClicksToTheHomeButton()
+        {
+            homeButton.Click();
+        }
+
+        [When(@"user clicks to the UI Testing button")]
+        public void WhenUserClicksToTheUITestingButton()
+        {
+            testButton.Click();
+        }
+
+        [When(@"user clicks to the Form button")]
+        public void WhenUserClicksToTheFormButton()
+        {
+            formButton.Click();
+        }
+        
+        [When(@"user clicks to the Error button")]
+        public void WhenUserClicksToTheErrorButton()
+        {
+            errorButton.Click();
+        }
+
+        [When(@"user type (.*) to the input field")]
+        public void WhenUserTypeToTheInputField(string inputText)
+        {
+            driver.FindElement(By.XPath("//input[@id='hello-input']")).SendKeys(inputText);
+        }
+        
+        [When(@"submit the form")]
+        public void WhenSubmitTheForm()
+        {
+            driver.FindElement(By.XPath("//button[@id='hello-submit']")).Submit();
+        }
+        
+        [Then(@"the Home button is active")]
+        public void ThenTheHomeButtonIsActive()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//li[@class='active']/a[@id='home']")));
+        }
+
+        [Then(@"the Home page loads")]
+        public void ThenTheHomePageLoads()
+        {
+            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.XPath("//img[@id='dh_logo']")));
+        }
+
+        [Then(@"the Form page loads")]
+        public void ThenTheFormPageLoads()
+        {
+            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='hello-input']")));
+        }
+        
+        [Then(@"the Form button is active")]
+        public void ThenTheFormButtonIsActive()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//li[@class='active']/a[@id='form']")));
+        }
+
+        [Then(@"the title is ""(.*)""")]
+        public void ThenTheTitleIs(string title)
+        {
+            String actualTitle = driver.Title;
+            Assert.AreEqual(title, actualTitle);
+        }
+
+        [Then(@"The Company Logo is visible on the page")]
+        public void ThenTheCompanyLogoIsVisibleOnThePage()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//img[@id='dh_logo']")));
+        }
+
+        [Then(@"user get a (.*) HTTP response code")]
+        public void ThenUserGetAHTTPResponseCode(int errorCode)
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath(String.Format("//h1[contains(.,'{0} Error: File not found :-(')]", errorCode))));
+        }
+        
+        [Then(@"""(.*)"" text is visible on the Home page in (.*) tag")]
+        public void ThenTextIsVisibleOnTheHomePage(string text, string tag)
+        {
+            IWebElement pageText = driver.FindElement(By.XPath(String.Format("//{0}[contains(.,'{1}')]", tag, text)));
+            String actualTag = pageText.TagName;
+            Assert.AreEqual(actualTag, tag);
+        }
+        
+        [Then(@"a form is visible")]
+        public void ThenAFormIsVisible()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//input[@id='hello-input']")));
+        }
+        
+        [Then(@"one input box is visible on the page")]
+        public void ThenOneInputBoxIsVisibleOnThePage()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//input[@id='hello-input']")));
+        }
+        
+        [Then(@"one submit button is visible on the page")]
+        public void ThenOneSubmitButtonIsVisibleOnThePage()
+        {
+            Assert.IsNotNull(driver.FindElement(By.XPath("//button[@id='hello-submit']")));
+        }
+        
+        [Then(@"the page redirected to the Hello page")]
+        public void ThenThePageRedirectedToTheHelloPage()
+        {
+            WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h1[@id='hello-text']")));
+            Assert.IsNotNull(driver.FindElement(By.XPath("//h1[@id='hello-text']")));
+        }
+
+        [Then(@"the following text is appear (.*)")]
+        public void ThenTheFollowingTextIsAppear(string text)
+        {
+            String actualText = driver.FindElement(By.XPath("//h1[@id='hello-text']")).Text;
+            Assert.AreEqual(text, actualText);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Close();
+        }
+    }
+}
